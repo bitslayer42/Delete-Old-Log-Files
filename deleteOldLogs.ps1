36374ede3be6ab5ab4220a094f11aa5fd9ecb1fb
Jon Wilson <tnk42@yahoo.com>
2018-03-12 08:34:28 -0400

    Update deleteOldLogs.ps1

--- deleteOldLogs.ps1
+++ deleteOldLogs.ps1
@@ -5,4 +5,5 @@ $Daysback = "-30"
 $CurrentDate = Get-Date
 $DatetoDelete = $CurrentDate.AddDays($Daysback)
 Get-ChildItem $Path | Where-Object { $_.LastWriteTime -lt $DatetoDelete } | Remove-Item #-WhatIf
+# Uncomment -WhatIf to see what files would be deleted
 
